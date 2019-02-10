77eb0f7e82526a2c9d18246f7f10360735c48e9f
Brandon Bakker <brandon@bdmd.com.au>
2019-02-10 11:17:49 +1000

    Removed old configs, updated install script

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@ ln -s ~/DotFiles/.tmux.conf ~/.tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm ~/.zshrc
 ln -s ~/DotFiles/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/sources/zsh-syntax-highlighting
 
 #neovim
+mkdir -p ~/.config/nvim/
 ln -s ~/DotFiles/init.vim ~/.config/nvim/init.vim
 
 #VimPlug for neovim
